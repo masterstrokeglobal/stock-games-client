5b161bafc7795da95cd06f7889fa063a2b2ec36e
kraftbase <132485941+kraftbase@users.noreply.github.com>
2025-02-20 11:31:26 +0530

    Dev (#45)
    * feat: add dynamic bonus
    
    * add wallet card
    
    * feat: add platform fees
    
    * fix: wallet calculation
    
    * feat: add lobby
    
    * feat: remove platform fees from withdrawl
    
    * feat: add agent password change
    
    * feat: add users and agent to company
    
    * feat: add company id to filters
    
    * feat: add company id to filters
    
    * fix: table overflow scroll
    
    * remove: lock file
    
    ---------
    
    Co-authored-by: Codezuma <patidarchandresh2002@gmail.com>
    Co-authored-by: chandresh-kraftbase <chandresh@kraftbase.com>

--- src/components/ui/data-table-server.tsx
+++ src/components/ui/data-table-server.tsx
@@ -19,6 +19,7 @@ import { cn } from "@/lib/utils";
 import ErrorBoundary from "@/components/ui/error-boundary";
 import Pagination from "./pagination";
 import { Skeleton } from "./skeleton";
+import { ScrollArea, ScrollBar } from "./scroll-area";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -73,78 +74,81 @@ export default function DataTable<TData, TValue>({
 
   return (
     <div>
-      <div className={cn("dark:bg-dark-primary border rounded-xl overflow-hidden bg-white", className)}>
-        <Table>
-          {showHeader && (
-            <TableHeader className="px-4">
-              {table?.getHeaderGroups().map((headerGroup) => (
-                <TableRow
-                  key={headerGroup.id}                >
-                  {headerGroup.headers.map((header) => (
-                    <TableHead key={header.id} className="text-sm  text-gray-800 dark-border  border-b font-medium whitespace-nowrap" >
-                      <span className="flex font-semibold">
-                        {header.isPlaceholder
-                          ? null
-                          : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                      </span>
-                    </TableHead>
-                  ))}
-                </TableRow>
-              ))}
-            </TableHeader>
-          )}
-          <TableBody  className="px-4">
-            {!loading &&
-              (table?.getRowModel().rows?.length ? (
-                table?.getRowModel().rows.map((row) => (
+      <div className={cn("dark:bg-dark-primary border rounded-xl  bg-white", className)}>
+        <ScrollArea type="always">
+          <Table>
+            {showHeader && (
+              <TableHeader className="px-4">
+                {table?.getHeaderGroups().map((headerGroup) => (
                   <TableRow
-                    className={cn("h-14", rowClassName)}
-                    key={row.id}
-                    data-state={row.getIsSelected() && "selected"}
-                  >
-                    {row?.getVisibleCells().map((cell) => (
-                      <ErrorBoundary key={cell.id}>
-                        <TableCell>
-                          {flexRender(
-                            cell?.column.columnDef.cell,
-                            cell.getContext()
-                          )}
-                        </TableCell>
-                      </ErrorBoundary>
+                    key={headerGroup.id}                >
+                    {headerGroup.headers.map((header) => (
+                      <TableHead key={header.id} className="text-sm  text-gray-800 dark-border  border-b font-medium whitespace-nowrap" >
+                        <span className="flex font-semibold">
+                          {header.isPlaceholder
+                            ? null
+                            : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                        </span>
+                      </TableHead>
                     ))}
                   </TableRow>
-                ))
-              ) : (
-                <TableRow>
-                  <TableCell
-                    colSpan={columns.length}
-                    className="h-24 text-center"
-                  >
-                    No results.
-                  </TableCell>
-                </TableRow>
-              ))}
-
-            {loading && (
-              <>
-                {Array(10)
-                  .fill(0)
-                  .map((_, i) => (
-                    <TableRow key={i} className=" p-1">
-                      {columns.map((column, j) => (
-                        <TableCell key={j}>
-                          <Skeleton className="h-6 w-full" />
-                        </TableCell>
+                ))}
+              </TableHeader>
+            )}
+            <TableBody className="px-4">
+              {!loading &&
+                (table?.getRowModel().rows?.length ? (
+                  table?.getRowModel().rows.map((row) => (
+                    <TableRow
+                      className={cn("h-14", rowClassName)}
+                      key={row.id}
+                      data-state={row.getIsSelected() && "selected"}
+                    >
+                      {row?.getVisibleCells().map((cell) => (
+                        <ErrorBoundary key={cell.id}>
+                          <TableCell>
+                            {flexRender(
+                              cell?.column.columnDef.cell,
+                              cell.getContext()
+                            )}
+                          </TableCell>
+                        </ErrorBoundary>
                       ))}
                     </TableRow>
-                  ))}
-              </>
-            )}
-          </TableBody>
-        </Table>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell
+                      colSpan={columns.length}
+                      className="h-24 text-center"
+                    >
+                      No results.
+                    </TableCell>
+                  </TableRow>
+                ))}
+
+              {loading && (
+                <>
+                  {Array(10)
+                    .fill(0)
+                    .map((_, i) => (
+                      <TableRow key={i} className=" p-1">
+                        {columns.map((column, j) => (
+                          <TableCell key={j}>
+                            <Skeleton className="h-6 w-full" />
+                          </TableCell>
+                        ))}
+                      </TableRow>
+                    ))}
+                </>
+              )}
+            </TableBody>
+          </Table>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
         <div className="flex md:items-center flex-wrap border-t py-2 px-4 bg-white w-full justify-between">
           <div className="py-1 min-h-[60px]   md:px-4 items-center flex flex-row w-fit  justify-between gap-2">
             <p className="text-[#6B7280]">Showing</p>
