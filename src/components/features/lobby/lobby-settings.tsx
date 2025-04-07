2201b14399b6a94d85e706d40344b242e448c831
Codezuma <patidarchandresh2002@gmail.com>
2025-04-07 14:44:41 +0530

    fix: lobby

--- src/components/features/lobby/lobby-settings.tsx
+++ src/components/features/lobby/lobby-settings.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { useAuthStore } from "@/context/auth-context";
 import Lobby, { LobbyGameType } from "@/models/lobby";
-import { useJoinLobby, useLeaveLobby, useReadyToPlay, useStartRound, useUnreadyToPlay } from "@/react-query/lobby-query";
+import { useJoinLobby, useLeaveLobby, useStartRound } from "@/react-query/lobby-query";
 import Link from "next/link";
 
 type Props = {
@@ -10,25 +10,17 @@ type Props = {
 
 const LobbySettings = ({ lobby }: Props) => {
     const { userDetails } = useAuthStore();
-    const { mutate: readyToPlay, isPending: isReadyLoading } = useReadyToPlay();
     const { mutate: joinLobby, isPending: isJoinLoading } = useJoinLobby();
     const {
         mutate: startRound,
         isPending: isStartLoading,
     } = useStartRound();
     const { mutate: leaveLobby, isPending: isLeaveLoading } = useLeaveLobby();
-    const { mutate: unreadyToPlay, isPending: isUnreadyLoading } = useUnreadyToPlay();
 
     // Assuming these methods exist on the Lobby model
     const userId = userDetails!.id!;
     const isParticipant = lobby.isParticipant(userId);
-    const isReady = lobby.isReadyToPlay(userId);
     const isLeader = lobby.isLeader(userId);
-
-
-    const handleReadyToPlay = () => {
-        readyToPlay(lobby.id!);
-    };
 
     const handleLeaveLobby = () => {
         leaveLobby(lobby.id!);
