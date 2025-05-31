8335743eb6e0cd069dd46597254c51248e4e9ae0
rajas-quanto <rajas@quanto-consulting.in>
2025-05-31 13:30:34 +0530

    fix

--- src/app/(locale)/game/(authorised)/redblack/page.tsx
+++ src/app/(locale)/game/(authorised)/redblack/page.tsx
@@ -5,20 +5,16 @@ import AdvertismentDialog from "@/components/features/advertisement/advertismemn
 // import LeaderBoard from "@/components/features/game/leaderboard";
 import Navbar from "@/components/features/game/navbar";
 import RedBlackRouletteGame from "@/components/features/game/red-black-roulette-game";
-import { MobileGameHeader } from "@/components/features/game/roulette-header";
+// import { MobileGameHeader } from "@/components/features/game/roulette-header";
 import UserWins from "@/components/features/game/user-wins-toggle";
-import HorseRace from "@/components/features/horse-animation/horse";
+// import HorseRace from "@/components/features/horse-animation/horse";
 import { useHorseRaceSound } from "@/context/audio-context";
-import {
-  useCurrentGame,
-  useGameState,
-  useIsPlaceOver,
-} from "@/hooks/use-current-game";
+import { useCurrentGame } from "@/hooks/use-current-game";
 // import useWindowSize from "@/hooks/use-window-size";
 import { cn } from "@/lib/utils";
-import { RoundRecord } from "@/models/round-record";
+// import { RoundRecord } from "@/models/round-record";
 import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
-import { useTranslations } from "next-intl";
+// import { useTranslations } from "next-intl";
 import CardSpinner from "@/components/features/card-spinner/card-spinner";
 import PlaceBets from "@/components/features/game/place-bet";
 import RedBlackLeaderBoard from "@/components/features/game/red-black-leaderboard";
@@ -53,7 +49,7 @@ const last10winners = [
 
 const GamePage = () => {
   const { roundRecord } = useCurrentGame();
-//   const { isMobile } = useWindowSize();
+  //   const { isMobile } = useWindowSize();
   useHorseRaceSound(roundRecord);
 
   return (
@@ -61,68 +57,66 @@ const GamePage = () => {
       <section className={cn("pt-14 min-h-screen w-screen")}>
         <Navbar />
         <UserWins />
-          <main className="grid grid-cols-12 gap-4 h-screen pt-8 px-4 pb-4 max-w-7xl mx-auto w-full">
-            {/* Left Column: Top (e.g. RouletteGame) and Bottom (e.g. CardSpinner) */}
-            <div className="col-span-12 lg:col-span-7 flex flex-col gap-4 h-full">
-              <div
-                className="max-h-[300px] bg-las relative rounded-2xl overflow-hidden w-full"
-                style={{
-                  flex: "0 0 50%", // 70% height
-                  ...borderStyle,
-                  backgroundImage: `url(/images/bg-lightning.jpg)`,
-                  backgroundSize: "100%",
-                }}
-              >
-                {roundRecord && (
-                  <RedBlackRouletteGame roundRecord={roundRecord} />
-                )}
-              </div>
-              <div
-                className="rounded-2xl overflow-y-auto"
-                style={{
-                  flex: "0 0 50%",
-                  ...borderStyle,
-                  backgroundImage: `url(/images/bg-design.jpg)`,
-                  backgroundSize: "10%",
-                }}
-              >
-                {roundRecord && (
-                  <CardSpinner
-                    last10winners={last10winners}
-                    // roundRecord={roundRecord}
-                  />
-                )}
-              </div>
+        <main className="grid grid-cols-12 gap-4 h-screen pt-8 px-4 pb-4 max-w-7xl mx-auto w-full">
+          {/* Left Column: Top (e.g. RouletteGame) and Bottom (e.g. CardSpinner) */}
+          <div className="col-span-12 lg:col-span-7 flex flex-col gap-4 h-full">
+            <div
+              className="max-h-[300px] bg-las relative rounded-2xl overflow-hidden w-full"
+              style={{
+                flex: "0 0 50%", // 70% height
+                ...borderStyle,
+                backgroundImage: `url(/images/bg-lightning.jpg)`,
+                backgroundSize: "100%",
+              }}
+            >
+              {roundRecord && (
+                <RedBlackRouletteGame roundRecord={roundRecord} />
+              )}
             </div>
+            <div
+              className="rounded-2xl overflow-y-auto"
+              style={{
+                flex: "0 0 50%",
+                ...borderStyle,
+                backgroundImage: `url(/images/bg-design.jpg)`,
+                backgroundSize: "10%",
+              }}
+            >
+              {roundRecord && (
+                <CardSpinner
+                  last10winners={last10winners}
+                  // roundRecord={roundRecord}
+                />
+              )}
+            </div>
+          </div>
 
-            {/* Right Column: Top (PlaceBets) and Bottom (LeaderBoard) */}
-            <div className="col-span-12 lg:col-span-5 flex flex-col gap-4 h-full w-full">
-              <div
-                className="rounded-2xl overflow-y-auto"
-                style={{
-                  flex: "0 0 70%",
-                  ...borderStyle,
-                  backgroundImage: `url(/images/bg-design.jpg)`,
-                  backgroundSize: "10%",
-                }}
-              >
-                {roundRecord && <PlaceBets roundRecord={roundRecord} />}
-              </div>
-              <div
-                className="rounded-2xl overflow-y-auto"
-                style={{
-                  flex: "0 0 30%",
-                  ...borderStyle,
-                  backgroundImage: `url(/images/bg-design.jpg)`,
-                  backgroundSize: "10%",
-                }}
-              >
-                {roundRecord && (
-                  <RedBlackLeaderBoard roundRecord={roundRecord} />
-                )}
-              </div>
+          {/* Right Column: Top (PlaceBets) and Bottom (LeaderBoard) */}
+          <div className="col-span-12 lg:col-span-5 flex flex-col gap-4 h-full w-full">
+            <div
+              className="rounded-2xl overflow-y-auto"
+              style={{
+                flex: "0 0 70%",
+                ...borderStyle,
+                backgroundImage: `url(/images/bg-design.jpg)`,
+                backgroundSize: "10%",
+              }}
+            >
+              {roundRecord && <PlaceBets roundRecord={roundRecord} />}
             </div>
-          </main>
+            <div
+              className="rounded-2xl overflow-y-auto"
+              style={{
+                flex: "0 0 30%",
+                ...borderStyle,
+                backgroundImage: `url(/images/bg-design.jpg)`,
+                backgroundSize: "10%",
+              }}
+            >
+              {roundRecord && <RedBlackLeaderBoard roundRecord={roundRecord} />}
+            </div>
+          </div>
+        </main>
         <AdvertismentDialog />
         <TawkMessengerReact
           propertyId="/67fcabcc5de05719072dd2b9"
@@ -137,98 +131,98 @@ const GamePage = () => {
 
 export default GamePage;
 
-const TimeLeft = ({ roundRecord }: { roundRecord: RoundRecord }) => {
-  const gameState = useGameState(roundRecord);
-  return gameState.placeTimeLeft.formatted;
-};
+// const TimeLeft = ({ roundRecord }: { roundRecord: RoundRecord }) => {
+//   const gameState = useGameState(roundRecord);
+//   return gameState.placeTimeLeft.formatted;
+// };
 
-const MobileGame = ({ roundRecord }: { roundRecord: RoundRecord }) => {
-  const isPlaceOver = useIsPlaceOver(roundRecord);
+// const MobileGame = ({ roundRecord }: { roundRecord: RoundRecord }) => {
+//   const isPlaceOver = useIsPlaceOver(roundRecord);
 
-  return (
-    <section className="text-game-text">
-      <MobileHeader roundRecord={roundRecord} />
-      {!isPlaceOver && (
-        <main className="bg-[#0A1634]">
-          <div className="px-2">
-            <div
-              className="rounded-xl overflow-hidden min-h-[300px] max-h-[400px]"
-              style={{
-                backgroundImage: `url(/images/bg-lightning.jpg)`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-                zIndex: 1000,
-              }}
-            >
-              {roundRecord && (
-                <RedBlackRouletteGame roundRecord={roundRecord} />
-              )}
-            </div>
-            {roundRecord && <CardSpinner last10winners={last10winners} />}
-            {/* {roundRecord && <CurrentBets round={roundRecord} />} */}
-          </div>
-        </main>
-      )}
-      {isPlaceOver && (
-        <>
-          {roundRecord && <RedBlackLeaderBoard roundRecord={roundRecord} />}
-          <div
-            className="rounded-xl overflow-hidden min-h-[300px] max-h-[400px] mx-2 mt-2"
-            style={{
-              backgroundImage: `url(/images/bg-lightning.jpg)`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-              zIndex: 1000,
-            }}
-          >
-            <RedBlackRouletteGame roundRecord={roundRecord} />
-          </div>
-          {roundRecord && <CardSpinner last10winners={last10winners} />}
-        </>
-      )}
-    </section>
-  );
-};
+//   return (
+//     <section className="text-game-text">
+//       <MobileHeader roundRecord={roundRecord} />
+//       {!isPlaceOver && (
+//         <main className="bg-[#0A1634]">
+//           <div className="px-2">
+//             <div
+//               className="rounded-xl overflow-hidden min-h-[300px] max-h-[400px]"
+//               style={{
+//                 backgroundImage: `url(/images/bg-lightning.jpg)`,
+//                 backgroundSize: "cover",
+//                 backgroundPosition: "center",
+//                 backgroundRepeat: "no-repeat",
+//                 zIndex: 1000,
+//               }}
+//             >
+//               {roundRecord && (
+//                 <RedBlackRouletteGame roundRecord={roundRecord} />
+//               )}
+//             </div>
+//             {roundRecord && <CardSpinner last10winners={last10winners} />}
+//             {/* {roundRecord && <CurrentBets round={roundRecord} />} */}
+//           </div>
+//         </main>
+//       )}
+//       {isPlaceOver && (
+//         <>
+//           {roundRecord && <RedBlackLeaderBoard roundRecord={roundRecord} />}
+//           <div
+//             className="rounded-xl overflow-hidden min-h-[300px] max-h-[400px] mx-2 mt-2"
+//             style={{
+//               backgroundImage: `url(/images/bg-lightning.jpg)`,
+//               backgroundSize: "cover",
+//               backgroundPosition: "center",
+//               backgroundRepeat: "no-repeat",
+//               zIndex: 1000,
+//             }}
+//           >
+//             <RedBlackRouletteGame roundRecord={roundRecord} />
+//           </div>
+//           {roundRecord && <CardSpinner last10winners={last10winners} />}
+//         </>
+//       )}
+//     </section>
+//   );
+// };
 
-const MobileHeader = ({ roundRecord }: { roundRecord: RoundRecord }) => {
-  const isPlaceOver = useIsPlaceOver(roundRecord);
-  const t = useTranslations("game");
+// const MobileHeader = ({ roundRecord }: { roundRecord: RoundRecord }) => {
+//   const isPlaceOver = useIsPlaceOver(roundRecord);
+//   const t = useTranslations("game");
 
-  if (isPlaceOver) {
-    return (
-      <>
-        <MobileGameHeader roundRecord={roundRecord} />
-        {roundRecord && (
-          <div className="mx-2 mt-2 rounded-xl overflow-hidden">
-            <CardSpinner
-              // roundRecord={roundRecord}
-              last10winners={last10winners}
-            />
-          </div>
-        )}
-        <div className="mx-2 mt-2 rounded-xl overflow-hidden">
-          <HorseRace roundRecord={roundRecord} />
-        </div>
-      </>
-    );
-  }
+//   if (isPlaceOver) {
+//     return (
+//       <>
+//         <MobileGameHeader roundRecord={roundRecord} />
+//         {roundRecord && (
+//           <div className="mx-2 mt-2 rounded-xl overflow-hidden">
+//             <CardSpinner
+//               // roundRecord={roundRecord}
+//               last10winners={last10winners}
+//             />
+//           </div>
+//         )}
+//         <div className="mx-2 mt-2 rounded-xl overflow-hidden">
+//           <HorseRace roundRecord={roundRecord} />
+//         </div>
+//       </>
+//     );
+//   }
 
-  return (
-    <header className="bg-background-game mx-2 flex justify-center flex-col text-center min-h-[20vh] rounded-xl">
-      {roundRecord && (
-        <div className="rounded-xl overflow-hidden">
-          <CardSpinner
-            // roundRecord={roundRecord}
-            last10winners={last10winners}
-          />
-        </div>
-      )}
-      <h1 className="mt-2">{t("round-starts-in")}</h1>
-      <p className="jersey text-6xl md:text-8xl leading-[3rem] md:leading-[5rem]">
-        <TimeLeft roundRecord={roundRecord!} />
-      </p>
-    </header>
-  );
-};
+//   return (
+//     <header className="bg-background-game mx-2 flex justify-center flex-col text-center min-h-[20vh] rounded-xl">
+//       {roundRecord && (
+//         <div className="rounded-xl overflow-hidden">
+//           <CardSpinner
+//             // roundRecord={roundRecord}
+//             last10winners={last10winners}
+//           />
+//         </div>
+//       )}
+//       <h1 className="mt-2">{t("round-starts-in")}</h1>
+//       <p className="jersey text-6xl md:text-8xl leading-[3rem] md:leading-[5rem]">
+//         <TimeLeft roundRecord={roundRecord!} />
+//       </p>
+//     </header>
+//   );
+// };
