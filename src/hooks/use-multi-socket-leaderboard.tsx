6a7e642bcd3c38541739584d08fea9ea6f47f0ad
lokesh <lokeshyadv8177@gmail.com>
2025-09-26 14:26:44 +0530

    fixes on stock leaderboard

--- src/hooks/use-multi-socket-leaderboard.tsx
+++ src/hooks/use-multi-socket-leaderboard.tsx
@@ -49,6 +49,11 @@ export const parseCOMEXMessage = (data: any): { [key: string]: number } => {
         }
 
         const fullSymbol = contractData[0];
+        
+        // Ignore symbols containing GCE26 (case insensitive)
+        if (fullSymbol.toLowerCase().includes("gce26")  || fullSymbol.toLowerCase().includes("sie26") ) {
+            continue;
+        }
 
         const symbolMatch = fullSymbol.match(
             /^([A-Z]{2,4})([FGHJKMNQUVXZ])([0-9]{2})$/
