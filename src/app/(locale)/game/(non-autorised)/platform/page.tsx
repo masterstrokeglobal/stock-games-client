6a80a4447db3c7004067e7a2b82ec242d9a7dca2
QuantoRahul <rahulrawat.quanto@gmail.com>
2025-10-08 16:31:21 +0530

    ADDED : removed mac88

--- src/app/(locale)/game/(non-autorised)/platform/page.tsx
+++ src/app/(locale)/game/(non-autorised)/platform/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import CategoryCarousel from "@/components/features/casino-games/category-carousel";
 import { CasinoProvidersCarousel } from "@/components/features/casino-games/game-providers";
-import GapGameCarousel from "@/components/features/casino-games/gap-game-carousel";
 import AdMarquee from "@/components/features/platform/ad-marquee";
 import CtaSection from "@/components/features/platform/cta-section";
 import WalletDialog from "@/components/features/platform/wallet-dialog";
@@ -49,7 +48,6 @@ const PlatformPage = () => {
                 <StockGameCarousel />
                 {isCasinoAllowed && (
                     <>
-                        <GapGameCarousel title="Mac88 Games" />
                         <CasinoProvidersCarousel title={tPlatform("game-providers")} />
                         <CategoryCarousel title={tPlatform("new-released")} new={true} direction="backward" />
                         <CategoryCarousel title={tPlatform("hot-games")} popular={true} direction="forward" />
